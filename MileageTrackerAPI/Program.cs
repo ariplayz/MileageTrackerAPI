9b4ca5b40fdd090f7f9dbb29f6827e342025cd68
Ari Cummings <ari@aricummings.com>
2025-08-21 23:07:44 -0400

    hi

--- MileageTrackerAPI/Program.cs
+++ MileageTrackerAPI/Program.cs
@@ -1,4 +1,3 @@
-
 namespace MileageTrackerAPI;
 
 public class Program
@@ -31,18 +30,18 @@ public class Program
         };
 
         app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast");
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
 
         app.Run();
     }
